15fd35f7c0b1fceb73c7096e6d5c0ec90bf1fbfa
Ismael Haddad <isma.7h@gmail.com>
2020-09-01 11:37:10 +0200

    Part 1 - Introduction

---
+++ nuxt.config.js
@@ -0,0 +1,48 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  mode: 'universal',
+  target: 'static',
+  head: {
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || '',
+      },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+  srcDir: 'src',
+  css: [],
+  plugins: [],
+  components: true,
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/vuetify',
+  ],
+  modules: [],
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: false,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+      },
+    },
+  },
+  build: {},
+}
---
+++ src/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <v-app> Hello World </v-app>
+</template>
